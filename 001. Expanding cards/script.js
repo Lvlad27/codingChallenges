e5ebc5b8d7fb5f4f36cf90b96af6b00302975aff
Vlad Leontescu <Lvlad27@gmail.com>
2021-11-11 00:29:59 +0200

    Finished second challenge, put some comments on the first

--- 001. Expanding cards/script.js
+++ 001. Expanding cards/script.js
@@ -12,3 +12,5 @@ function removeActiveClasses() {
         panel.classList.remove("active");
     });
 }
+
+// practic este vorba despre cum se schimba elementele de html/css pentru un event dat (e.g. 'click'). Se stabilesc niste proprietati default si niste proprietati pentru event-ul 'click'. Cand event-ul 'click' este prezent pe element, toate celelalte elemente iau proprietatile default. 
